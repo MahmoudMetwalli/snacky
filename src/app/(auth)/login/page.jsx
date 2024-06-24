ecbcf16ea9015c59fcdde491474fb82a06a11ddc
MahmoudMetwalli <mammkbih@gmail.com>
2024-06-25 01:12:02 +0300

    2024-06-25 01:12:02 AM

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,11 +1,7 @@
 import LogInForm from '@/components/logInForm/logInForm';
 import styles from './login.module.css';
-import { cookies } from "next/headers";
 
 export default function LogInPage() {
-	const cookieStore = cookies();
-	console.log(cookieStore);
-
 	return (
 		<div className={styles.container}>
 			<h1>Sign in</h1>
