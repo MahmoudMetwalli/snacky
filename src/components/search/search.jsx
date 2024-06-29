3965f1a181ae5bea64e8f87a68aa767d53e1d2b0
RamzyAR7 <rameya683@gmail.com>
2024-06-29 03:50:02 +0300

    ramzy

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -3,13 +3,17 @@ import styles from './search.module.css';
 
 const SearchBar = ({ value, onChange }) => {
     return (
-        <input
-            type="text"
-            className={styles.searchInput}
-            placeholder="Search for products..."
-            value={value}
-            onChange={(e) => onChange(e.target.value)}
-        />
+        <div className={styles.searchInputContainer} >
+            <div className={styles.searchInput}>
+                <input
+                    type="text"
+                    className={styles.searchInput}
+                    placeholder="Search for products..."
+                    value={value}
+                    onChange={(e) => onChange(e.target.value)}
+                />
+            </div>
+        </div>
     );
 };
 
