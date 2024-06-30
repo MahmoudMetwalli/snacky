c5a148bdacf81daae2b8edaaae72bcaa29de24b4
MahmoudMetwalli <mammkbih@gmail.com>
2024-06-30 18:49:20 +0300

    2024-06-30 06:49:20 PM

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -6,13 +6,13 @@ const About = () => {
         <div className={styles.container}>
             <h1 className={styles.title}>About Us</h1>
             <p className={styles.description}>
-                Welcome to Snacky, your number one source for all Healthy food. We`&apos;`re dedicated to giving you the very best of Healthy food (sancks), with a focus on quality, freshness, and customer service.
+                Welcome to Snacky, your number one source for all Healthy food. We&apos;re dedicated to giving you the very best of Healthy food (sancks), with a focus on quality, freshness, and customer service.
             </p>
             <p className={styles.description}>
-                Founded in 2023, Snacky has come a long way from its beginnings. When we first started out, our passion for healthy and delicious snacks drove us to do tons of research so that Snacky can offer you the world`&apos;`s most advanced snacking options. We now serve customers all over the country and are thrilled that we`&apos;`re able to turn our passion into our own website.
+                Founded in 2023, Snacky has come a long way from its beginnings. When we first started out, our passion for healthy and delicious snacks drove us to do tons of research so that Snacky can offer you the world&apos;s most advanced snacking options. We now serve customers all over the country and are thrilled that we&apos;re able to turn our passion into our own website.
             </p>
             <p className={styles.description}>
-                We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don`&apos;`t hesitate to contact us.
+                We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don&apos;t hesitate to contact us.
             </p>
             <p className={styles.signature}>
                 Sincerely,<br />
--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -4,7 +4,34 @@ import styles from './admin.module.css';
 
 
 export default function AdminPage() {
+  const users = [
+    {username: 'Ali', email: 'Ali@mail.com'},
+    {username: 'Ahmed', email: 'Ahmed@mail.com'},
+    {username: 'Luke', email: 'Luke@mail.com'},
+    {username: 'Sadek', email: 'Sadek@mail.com'},
+    {username: 'Iris', email: 'Iris@mail.com'},
+  ]
+  const orders = [
+    {id: 1,orderuser: 'Ali'},
+    {id: 2,orderuser: 'Ahmed'},
+    {id: 3,orderuser: 'Luke'},
+    {id: 4,orderuser: 'Sadek'},
+    {id: 5,orderuser: 'Iris'},
+  ]
   return(
-	<div className={styles.container}></div>
+	<div className={styles.container}>
+    <div className={styles.usersContainer}>Users:{users.map((user) => (
+        <div className={styles.users} key={user.username}>User Name:{user.username}
+        <p>User E-mail:{user.email}</p>
+        <button className={styles.deleteButton}>Delete</button>
+        </div>
+    ))}</div>
+    <div className={styles.ordersContainer}>Orders:{orders.map((order) => (
+        <div className={styles.orders} key={order.orderuser}>Order ID: {order.id}
+        <p>Ordered By: {order.orderuser}</p>
+        <button className={styles.deleteButton}>Delete</button>
+        </div>
+    ))}</div>
+  </div>
   )
 }
