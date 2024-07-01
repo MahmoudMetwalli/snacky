bb6cec08cfb369230ef7fd5d9fa4f150655f4941
RamzyAR7 <rameya683@gmail.com>
2024-07-02 01:57:00 +0300

    ramzy

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,10 +1,8 @@
 import LogInForm from '@/components/logInForm/logInForm';
-import styles from './login.module.css';
 
 export default function LogInPage() {
 	return (
-		<div className={styles.container}>
-			<h1>Sign in</h1>
+		<div>
 			<LogInForm />
 		</div>
 	)
--- src/components/logInForm/logInForm.jsx
+++ src/components/logInForm/logInForm.jsx
@@ -14,11 +14,17 @@ export default function LogInForm() {
   useEffect(() => {
 		state?.success && router.push("/");
 	  }, [state?.success, router]);
-  return (<form className={styles.form} action={formAction}>
-	<input type="text" placeholder="E-Mail Address" name="email"/>
-	<input type="password" placeholder="Password" name="password"/>
-	<p>You don&apos;t have an account ?<Link href='/register' className={styles.signup}>  Click here to sign up </Link></p>
-	{ state?.error }
-	<button>Log in</button>
-	</form>)
+
+	return (
+		<div className={styles['form-container']}>
+			<h1>Login</h1>
+			<form className={styles.form} action={formAction}>
+				<input type="email" placeholder="E-Mail Address" name="email" required />
+				<input type="password" placeholder="Password" name="password" required />
+				<p>You don&apos;t have an account? <Link href='/register' className={styles.signup}>Click here to sign up</Link></p>
+				{state?.error && <p className={styles.error}>{state?.error}</p>}
+				<button type="submit">Log in</button>
+			</form>
+		</div>
+	);
 }
