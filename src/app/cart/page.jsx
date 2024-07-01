f93ee01b852c64b11d9200b0a279f63a84ca5696
RamzyAR7 <rameya683@gmail.com>
2024-07-02 01:01:27 +0300

    ramzy

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -1,6 +1,5 @@
 "use client";
 import Image from 'next/image';
-import styles from './cart.module.css';
 import { useContext } from 'react';
 import CartContext from '@/context/cartContext';
 import Cart from '@/components/cart/cart';
--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -4,45 +4,48 @@ import styles from './cart.module.css';
 import { useContext } from 'react';
 import CartContext from '@/context/cartContext';
 
-
 export default function Cart() {
-    const { deleteItemFromCart, addItemToCart, cart } = useContext(CartContext);
+	const { deleteItemFromCart, addItemToCart, cart } = useContext(CartContext);
+
 	const increaseQty = (cartItem) => {
-		const newQty = cartItem?.quantity + 1;
-		const item = { ...cartItem, quantity: newQty };
-	
+		const newQty = cartItem.quantity + 1;
 		if (newQty > Number(cartItem.stock)) return;
-	
-		addItemToCart(item);
-	  };
-	
-	  const decreaseQty = (cartItem) => {
-		const newQty = cartItem?.quantity - 1;
-		const item = { ...cartItem, quantity: newQty };
-	
+		addItemToCart({ ...cartItem, quantity: newQty });
+	};
+
+	const decreaseQty = (cartItem) => {
+		const newQty = cartItem.quantity - 1;
 		if (newQty <= 0) return;
-	
-		addItemToCart(item);
-	  };
-	  const totalAmount = cart?.cartItems?.reduce(
-		(acc, item) => acc + item.quantity * item.price,
-		0
-	  );
-	return(
-	<div className={styles.container}>
-		<div className={styles.cart}>
-			{cart?.cartItems?.map((item) => (<div key={item.id} className={styles.cartItem}>
-			<Image src={item.photo} alt={item.name} width={200} height={200} />
-			<p>{item.name}</p>
-			<span>Price per single order: {item.price}.L.E</span>
-			<div className={styles.amount}><button onClick={() => increaseQty(item)}>+</button><span>{item.quantity}</span><button onClick={() => decreaseQty(item)}>-</button></div>
-			<span>Total: {item.price * item.quantity} .L.E</span>
-			<button className={styles.removeButton } onClick={() => deleteItemFromCart(item.id)}>Remove</button>
-			</div>))}
+		addItemToCart({ ...cartItem, quantity: newQty });
+	};
+
+	const totalAmount = cart?.cartItems?.reduce((acc, item) => acc + item.quantity * item.price, 0);
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.cart}>
+				{cart?.cartItems?.map((item) => (
+					<div key={item.id} className={styles.cartItem}>
+						<Image src={item.photo} alt={item.name} width={200} height={200} />
+						<p>{item.name}</p>
+						<span>Price per single order: {item.price} L.E</span>
+						<div className={styles.amount}>
+							<button onClick={() => decreaseQty(item)}>-</button>
+							<span>{item.quantity}</span>
+							<button onClick={() => increaseQty(item)}>+</button>
+						</div>
+						<span>Total: {item.price * item.quantity} L.E</span>
+						<button className={styles.removeButton} onClick={() => deleteItemFromCart(item.id)}>Remove</button>
+					</div>
+				))}
+			</div>
+			<div className={styles.total}>
+				<h1>Total Amount: {totalAmount} L.E</h1>
+				<div className={styles.purchase}>
+					<button className={styles.payButton}>Save and Pay</button>
+					<button className={styles.discardButton}>Discard Cart</button>
+				</div>
+			</div>
 		</div>
-		<div className={styles.total}>
-			<h1>Total Amount: {totalAmount} .L.E</h1>
-			<div className={styles.purchase}><button className={styles.payButton}>Save and Pay</button><button className={styles.discardButton}>Discard Cart</button></div>
-		</div>
-	</div>)
+	);
 }
