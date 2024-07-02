41e54fa234711fca424d964020ff64f59fc83d2f
MahmoudMetwalli <mammkbih@gmail.com>
2024-07-03 00:52:34 +0300

    2024-07-03 12:52:34 AM

--- src/components/myOrders/myOrders.jsx
+++ src/components/myOrders/myOrders.jsx
@@ -1,10 +1,11 @@
 'use client';
+import Link from 'next/link';
 import styles from './myOrders.module.css';
 import Image from 'next/image';
 
 
 export default function MyOrders({ myOrders }) {
-  return(<div className={styles.container}>{myOrders.map((order) => (<div><span>Order ID: {order.id}</span>{((JSON.parse(order.json)).cartItems).map((item) => (<div key={item.id} className={styles.cartItem}>
+  if (myOrders.length !== 0) { return(<div className={styles.container}>{myOrders.map((order) => (<div><span>Order ID: {order.id}</span>{((JSON.parse(order.json)).cartItems).map((item) => (<div key={item.id} className={styles.cartItem}>
     <Image src={item.photo} alt={item.name} width={200} height={200} />
     <p>{item.name}</p>
     <span>Price per single order: {item.price} L.E</span>
@@ -14,5 +15,6 @@ export default function MyOrders({ myOrders }) {
     <span>Total: {item.price * item.quantity} L.E</span>
   </div>))}</div>))}
       </div>
-    );
+    )} else {return(<div className={styles.container}><Link href='/order'> Make your first order frim here</Link></div>
+    )}
 }
