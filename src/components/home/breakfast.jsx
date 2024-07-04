15ec077712e4d0c8ee82ecf4322136bca4b288a4
RamzyAR7 <rameya683@gmail.com>
2024-07-04 17:40:11 +0300

    ramzy

--- src/components/home/breakfast.jsx
+++ src/components/home/breakfast.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './breakfast.module.css';
 import Link from 'next/link';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const BreakfastSection = () => {
+    useEffect(() => {
+        AOS.init({
+            duration: 500,  // Adjust the duration for faster animations (in milliseconds)
+            once: true,
+        });
+    }, []);
+
     return (
-        <div className={styles.container}>
-            <div className={styles.textContainer}>
+        <div className={styles.container} data-aos="fade-up">
+            <div className={styles.textContainer} data-aos="fade-right">
                 <h1>Breakfast<br />To-Go</h1>
                 <p>No time to linger? That's fine. We've packed you a breakfast fit for kings. Emphasis on "fit."<br />
                     Check out the ingredients:</p>
@@ -20,7 +29,7 @@ const BreakfastSection = () => {
                     <button className={styles.orderButton}>ORDER NOW !!!</button>
                 </Link>
             </div>
-            <div className={styles.imageContainer}>
+            <div className={styles.imageContainer} data-aos="fade-left">
                 <img src="/breakfast3.png" alt="Breakfast jars" />
             </div>
         </div>
