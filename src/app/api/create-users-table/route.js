d0b4c3a95fe2885b60b4c61511c50125b99d6d55
MahmoudMetwalli <mammkbih@gmail.com>
2024-07-04 21:47:55 +0300

    2024-07-04 09:47:55 PM

--- src/app/api/create-users-table/route.js
+++ src/app/api/create-users-table/route.js
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function GET(request) {
   try {
 	const result =
-	  await sql`CREATE TABLE IF NOT EXISTS "users" ( username VARCHAR(256) NOT NULL, password VARCHAR(256) NOT NULL, email VARCHAR(256) NOT NULL, userorders INT);`;
+	  await sql`ALTER TABLE users ADD adress VARCHAR;`;
 	return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
 	return NextResponse.json({ error }, { status: 500 });
