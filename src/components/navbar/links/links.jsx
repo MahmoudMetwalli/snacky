37d18674a39793c42c0060d7526c903677e10698
RamzyAR7 <rameya683@gmail.com>
2024-07-05 21:53:01 +0300

    ramzy

--- src/components/navbar/links/links.jsx
+++ src/components/navbar/links/links.jsx
@@ -1,113 +1,137 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import styles from './links.module.css';
 import NavLink from './navLink/navLink';
 import Image from 'next/image';
 import { logOut } from '@/lib/action';
-import { useFormState } from "react-dom";
-import { useContext, useEffect } from 'react';
 import CartContext from '@/context/cartContext';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faHistory, faSignOutAlt, faCog } from '@fortawesome/free-solid-svg-icons';
 
 const links = [
-  {
-    title: 'Home',
-    path: '/'
-  },
-  {
-    title: 'Contact',
-    path: '/contact'
-  },
-  {
-    title: 'Order',
-    path: '/order'
-  },
-
+  { title: 'Home', path: '/' },
+  { title: 'Contact', path: '/contact' },
+  { title: 'Order', path: '/order' },
 ];
+
 const auths = [
-  {
-    title: 'LogIn',
-    path: '/login'
-  },
-  {
-    title: 'Register',
-    path: '/register'
-  }];
+  { title: 'LogIn', path: '/login' },
+  { title: 'Register', path: '/register' },
+];
 
 const Links = ({ session }) => {
   const { deleteItemFromCart, addItemToCart, cart } = useContext(CartContext);
-  const [state, formAction] = useFormState(logOut, undefined);
   const router = useRouter();
-  useEffect(() => {
-		if (state?.success) {
-			router.push("/login");
-		}
-	}, [state?.success, router]);
-
   const [open, setOpen] = useState(false);
-  // TEMPORARY
+  const [profileOpen, setProfileOpen] = useState(false);
+
+  useEffect(() => {
+    if (!session) {
+      router.push("/login");
+    }
+  }, [session, router]);
+
   let admin = false;
   if (session) {
-   admin = session.user.admin;
+    admin = session.user.admin;
   }
+
+  const handleLogout = async () => {
+    await logOut();
+    router.push("/login");
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.links}>
-        {session && session.user ? (<div className={styles.hello}>Hi There<br></br>{session.user.firstName}&nbsp;{session.user.lastName}</div>) : (<div></div>)}
+        {session && session.user ? (
+          <div className={styles.hello}>Hi There<br />{session.user.firstName} {session.user.lastName}</div>
+        ) : (
+          <div></div>
+        )}
         {links.map(link => (
           <NavLink item={link} key={link.title} />
         ))}
-        {session
-          ? (
-            <div className={styles.all}>
-              <NavLink item={{ title: 'My Profile', path: '/myprofile' }} />
-              {admin && <NavLink item={{ title: 'Admin', path: '/admin' }} />}
-            </div>
-            )
-          : (<>{auths.map(auth => (
-            <NavLink item={auth} key={auth.title} />
-          ))}</>)}
+        {session ? (
           <div className={styles.all}>
-          <div className={styles.cart}>
-        <Link  href='/cart' className={styles.cart}><Image src='/shopping.png' alt='shopping' width={40} height={40} />
-         </Link>
-         <span>{cart?.cartItems?.length || 0}</span>
-        </div>
-          {session && <form action={formAction}>
-            <button className={styles.logout} type='submit'>Log Out</button>
-            </form>}
+            {admin && <NavLink item={{ title: 'Admin', path: '/admin' }} />}
           </div>
+        ) : (
+          auths.map(auth => (
+            <NavLink item={auth} key={auth.title} />
+          ))
+        )}
+        <div className={styles.all}>
+          <div className={styles.cart}>
+            <Link href='/cart' className={styles.cart}>
+              <Image src='/shopping.png' alt='shopping' width={40} height={40} />
+            </Link>
+            <span>{cart?.cartItems?.length || 0}</span>
+          </div>
+          {session && (
+            <div className={styles.profileIcon} onClick={() => setProfileOpen(prev => !prev)}>
+              <Image src='/icons8-user-100.png' alt='Profile' width={40} height={40} />
+              {profileOpen && (
+                <div className={styles.dropdownMenu}>
+                  <Link href='/myprofile'>
+                    <button>
+                      <FontAwesomeIcon icon={faCog} className={styles.icon} /> Setting
+                    </button>
+                  </Link>
+                  <hr className={styles.separator} />
+                  <Link href='/orderhistory'>
+                    <button>
+                      <FontAwesomeIcon icon={faHistory} className={styles.icon} /> Order History
+                    </button>
+                  </Link>
+                  <hr className={styles.separator} />
+                  <button className={styles.logout} onClick={handleLogout}>
+                    <FontAwesomeIcon icon={faSignOutAlt} className={styles.icon} /> Log Out
+                  </button>
+                </div>
+              )}
+            </div>
+          )}
+        </div>
       </div>
-      <Image className={styles.menuButton} src='/newmenu.png' alt='' height={30} width={30} onClick={() => setOpen(prev => !prev)}/>
-      { open && (<div className={styles.mobileLinks}>
-        {session && session.user ? (<div className={styles.hello}>Hi There<br></br>{session.user.firstName}&nbsp;{session.user.lastName}</div>) : (<div></div>)}
-        {links.map(link => (
-          <NavLink item={link} key={link.title} />
-        ))}
-        {session
-          ? (
+      <Image className={styles.menuButton} src='/newmenu.png' alt='' height={30} width={30} onClick={() => setOpen(prev => !prev)} />
+      {open && (
+        <div className={styles.mobileLinks}>
+          {session && session.user ? (
+            <div className={styles.hello}>Hi There<br />{session.user.firstName} {session.user.lastName}</div>
+          ) : (
+            <div></div>
+          )}
+          {links.map(link => (
+            <NavLink item={link} key={link.title} />
+          ))}
+          {session ? (
             <div className={styles.allMobile}>
               <NavLink item={{ title: 'My Profile', path: '/myprofile' }} />
               {admin && <NavLink item={{ title: 'Admin', path: '/admin' }} />}
             </div>
-            )
-          : (<>{auths.map(auth => (
-            <NavLink item={auth} key={auth.title} />
-          ))}</>)}
+          ) : (
+            auths.map(auth => (
+              <NavLink item={auth} key={auth.title} />
+            ))
+          )}
           <div className={styles.all}>
-          <div className={styles.cart}>
-        <Link  href='/cart' className={styles.cart}><Image src='/shopping.png' alt='shopping' width={40} height={40} />
-         </Link>
-         <span>{cart?.cartItems?.length || 0}</span>
+            <div className={styles.cart}>
+              <Link href='/cart' className={styles.cart}>
+                <Image src='/shopping.png' alt='shopping' width={40} height={40} />
+              </Link>
+              <span>{cart?.cartItems?.length || 0}</span>
+            </div>
+            {session && (
+              <button className={styles.logout} onClick={handleLogout}>
+                <FontAwesomeIcon icon={faSignOutAlt} /> Log Out
+              </button>
+            )}
+          </div>
         </div>
-          {session && <form action={formAction}>
-            <button className={styles.logout} type='submit'>Log Out</button>
-            </form>}
-          </div>
-          </div>
       )}
     </div>
   );
