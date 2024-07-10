29adeb9893861429bd78a2eb15fbc479e9a8fe8e
MahmoudMetwalli <mammkbih@gmail.com>
2024-07-10 18:15:42 +0300

    2024-07-10 06:15:42 PM

--- src/components/menuGetter/menuGetter.jsx
+++ src/components/menuGetter/menuGetter.jsx
@@ -1,5 +1,6 @@
+
 import { getMenu } from '@/lib/action';
-import Order from '../order/page';
+import Order from '../order/order';
 export default async function MenuGetter() {
   const menu = await getMenu();
   return(<Order menu={menu}/>
